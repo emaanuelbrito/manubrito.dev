417ab579178aeacc1284f99ffa0f344dee0bd100
Emanuel Josue Brito Rodriguez <ing.emanuelbrito@gmail.com>
2025-05-12 18:29:44 -0400

    seccion principal, proyectos, estudios y sobre mi completadas

--- app/app.py
+++ app/app.py
@@ -31,7 +31,34 @@ def after_request(response):
 @app.route("/")
 def index():
     try:
+        # 1. Bring the data
         context = get_all_data()
+        projects = context["projects"]
+        technologies = context["technologies"]
+        projects_technologies = context["projects_technologies"]
+
+        # 2. Badge limit
+        display_limit = 2
+
+        # 3. Tech list for each project
+        for project in projects:
+            # 3.1. IDs for used techs in this project
+            tech_ids = [
+                project_technology["technology_id"]
+                for project_technology in projects_technologies
+                if project_technology["project_id"] == project["id"]
+            ]
+            # 3.2. Filtered tech
+            project["techs"] = [
+                tech for tech in technologies
+                if tech["id"] in tech_ids
+            ]
+
+        # 4. Insert processed data into context
+        context["projects"] = projects
+        context["display_limit"] = display_limit
+
+        # 5. Calculate experience and other variables
         start_date = date(2024, 9, 1)
         experience = calculate_experience(start_date)
         context.update({"experience": experience, "current_date": date.today()})
@@ -39,7 +66,7 @@ def index():
     except Exception as e:
         logger.error(f"[SERVER]: Error retrieving data")
         return render_template("index.html", context=(jsonify({"error": "Error retrieving data"}))), 500
-
+    
 
 if __name__ == "__main__":
     app.run(debug=True, port=4000)
